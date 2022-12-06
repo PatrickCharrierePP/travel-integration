2e94ea843a80e320dcdaa3f6f8485e448dc50c85
PatrickCharrierePP <117608578+PatrickCharrierePP@users.noreply.github.com>
2022-12-06 17:55:56 +1100

    Display product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductComponent,
+    ProductListComponent,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,8 +1,13 @@
+export interface Image {
+  src: string;
+}
+
 export interface Product {
   id: number;
   name: string;
   price: number;
   description: string;
+  img: Image;
 }
 
 export const products = [
@@ -10,18 +15,27 @@ export const products = [
     id: 1,
     name: 'Deluxe double room',
     price: 299,
-    description: 'A large bedroom with one of the best view over the city'
+    description: 'A large bedroom with one of the best view over the city',
+    img: {
+      src: 'https://danaberez.com/wp-content/uploads/2019/01/Where-to-Stay-in-NYC-Hotel-Indigo-Lower-East-Side.jpg',
+    },
   },
   {
     id: 2,
     name: 'Deluxe simple loft',
     price: 449,
-    description: '1 Double bedroom in a loft environment with view'
+    description: '1 Double bedroom in a loft environment with view',
+    img: {
+      src: 'https://media-cdn.tripadvisor.com/media/photo-s/1c/d7/6e/56/interior-view-of-lounge.jpg',
+    },
   },
   {
     id: 3,
     name: 'Deluxe double loft',
     price: 499,
-    description: '2 Double bedroom in a loft environment with ocean view'
-  }
+    description: '2 Double bedroom in a loft environment with ocean view',
+    img: {
+      src: 'https://cdn.concreteplayground.com/content/uploads/2022/04/bondi-view_airbnb-1-1920x1080.jpg',
+    },
+  },
 ];
