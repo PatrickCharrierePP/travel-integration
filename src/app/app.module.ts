04c07d509d73b89a5e23582bae95c592f81a75f1
PatrickCharrierePP <117608578+PatrickCharrierePP@users.noreply.github.com>
2022-12-07 15:08:20 +1100

    Add angular material and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -9,12 +9,25 @@ import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const routes: Routes = [
+  { path: 'home', component: CheckoutComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'product-list', component: ProductListComponent },
+  { path: '**', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   declarations: [
     AppComponent,
